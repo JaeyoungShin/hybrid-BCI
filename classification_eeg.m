f20feee8eacb696fc9af78c94219f2b2175b52cf
JaeyoungShin <naraeshigo@gmail.com>
2016-07-30 22:12:30 +0200

    Update classification_eeg.m

--- classification_eeg.m
+++ classification_eeg.m
@@ -2,7 +2,7 @@
 % All MATLAB functions are available in BBCI toolbox
 % Some minor code modifications might be applied
 % We do not guarantee all of functions works properly in your platform
-% If you want to see more tutorials, visit BBCI toolbox (https://github.com/bbci/bbci_public)
+% For more tutorials, visit BBCI toolbox (https://github.com/bbci/bbci_public)
 
 % specify your eeg data directory (EegMyDataDir) and temporary directory (TemDir)
 startup_bbci_toolbox('DataDir',EegMyDataDir,'TmpDir','/tmp/');
